3dd1b2efd827649b4b2a9acdf170cb0cbb3ef933
amina.saif0 <amina.saif@pakeventures.com>
2023-12-08 16:51:18 +0500

    initialized ATM with total balance and made checks for transactions made by user according to available funds in the atm

--- ATM.rb
+++ ATM.rb
@@ -1,27 +1,32 @@
-require_relative 'user'
-
-class ATM < User
-    def initialize
-        total_balance = 100000
-        transaction_limit = 20000
+class ATM
+    def initialize(balance, limit)
+        @total_balance = balance
+        @transaction_limit = limit
     end
     
-    def verified?(pin)
-        self.pin == pin ? true : self.freeze
-    end
-    
-    def get_balance
-        puts "#{owner_name} your balance is #{balance}"
+    def self.get_balance
+        puts "Total availble balance is #{balance}"
         puts "Please recharge your account. Your balance is #{balance}" if balance <= 0
     end
 
-    def withdraw(amount)
+    def total_balance
+        @total_balance
+    end
+    def transaction_limit
+        @transaction_limit
+    end
+
+    def self.set_balance(symbol, amount)
+        eval("@balance #{symbol}= #{amount}")
+    end
+
+    def self.withdraw(amount)
         puts "***WARNING*** you're trying to make transaction for an amount which is not available to you." if amount > balance
         set_balance(symbol "-", amount)
         get_balance
     end
 
-    def deposit(amount)
+    def self.deposit(amount)
         puts "You are depositing a total of #{amount}"
         set_balance(symbol "+", amount)
         get_balance
