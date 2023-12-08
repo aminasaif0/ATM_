53c766d2b1c4f0f50b7115b7eca3a691bf9bcd6d
amina.saif0 <amina.saif@pakeventures.com>
2023-12-08 12:22:37 +0500

    Indexing Issue resolved with has maps

--- reader.rb
+++ reader.rb
@@ -1,30 +1,19 @@
 require_relative 'user'
+
 class Reader
-  INDEX_MAP = { account_number: 0, balance: 1, pin: 2, owner_name: 3 }
 
-  def self.read_user_from_file(file_path)
-    lines = File.readlines(file_path).map(&:chomp)
-    user_data_array = []
-
-    INDEX_MAP.each do |key, index|
-      value = lines[index]
-      
-      converted_value = if key == :account_number || key == :pin
-                          value.to_i
-                        elsif key == :balance
-                          value.to_f
-                        else
-                          value
-                        end
-
-      user_data_array << converted_value
+  INDEX_MAP = {:acc => 0, :balance => 1, :pin => 2, :owner_name => 3}
+    def self.read_user_from_file(file_path)
+        lines = File.readlines(file_path).map(&:chomp)
+        account_number = lines[INDEX_MAP[:acc]].to_i
+        balance = lines[INDEX_MAP[:balance]].to_f
+        pin = lines[INDEX_MAP[:pin]].to_i
+        owner_name = lines[INDEX_MAP[:owner_name]]
+        userDataArray = [account_number, balance, pin, owner_name]
     end
 
-    user_data_array
-  end
-
-  def self.user_details
-    file_path = 'user_data.txt'
-    read_user_from_file(file_path)
-  end
+    def self.user_details
+        file_path = 'user_data.txt'
+        read_user_from_file(file_path)
+    end
 end
