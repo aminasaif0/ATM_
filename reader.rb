ff3f40d516b83913acf7912757d939732fca7b63
amina.saif0 <amina.saif@pakeventures.com>
2023-12-08 11:28:57 +0500

    made hash map for index and initialized returning array with converted values

--- reader.rb
+++ reader.rb
@@ -1,18 +1,30 @@
 require_relative 'user'
-
 class Reader
-    
-    def self.read_user_from_file(file_path)
-        lines = File.readlines(file_path).map(&:chomp)
-        account_number = lines[0].to_i
-        balance = lines[1].to_f
-        pin = lines[2].to_i
-        owner_name = lines[3]
-        userDataArray = [account_number, balance, pin, owner_name]
+  INDEX_MAP = { account_number: 0, balance: 1, pin: 2, owner_name: 3 }
+
+  def self.read_user_from_file(file_path)
+    lines = File.readlines(file_path).map(&:chomp)
+    user_data_array = []
+
+    INDEX_MAP.each do |key, index|
+      value = lines[index]
+      
+      converted_value = if key == :account_number || key == :pin
+                          value.to_i
+                        elsif key == :balance
+                          value.to_f
+                        else
+                          value
+                        end
+
+      user_data_array << converted_value
     end
 
-    def self.user_details
-        file_path = 'user_data.txt'
-        read_user_from_file(file_path)
-    end
+    user_data_array
+  end
+
+  def self.user_details
+    file_path = 'user_data.txt'
+    read_user_from_file(file_path)
+  end
 end
