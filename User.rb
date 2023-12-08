eee3c579f266a63d42f44b11579018779d6dfc53
amina.saif0 <amina.saif@pakeventures.com>
2023-12-08 09:47:23 +0500

    removed extra space from User.rb

--- User.rb
+++ User.rb
@@ -1,4 +1,3 @@
-
 class User
     def initialize(owner_name, pin, balance, account_number)
         @owner_name = owner_name
@@ -18,18 +17,5 @@ class User
 
     def to_s
         "Account Number: #{@account_number}, Balance: #{@balance}, Owner Name: #{@owner_name}"
-      end
-  
-    def self.read_from_file(file_path)
-        lines = File.readlines(file_path).map(&:chomp)
-        account_number = lines[0].to_i
-        balance = lines[1].to_f
-        pin = lines[2].to_i
-        owner_name = lines[3]
-        User.new(owner_name, pin, balance, account_number)
     end
-
-    file_path = 'user_data.txt'
-    user_instance = User.read_from_file(file_path)
-    puts user_instance
 end 
